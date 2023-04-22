ceecc079d353fed2c68c1ddf7f1309ce716bd7eb
vilayat-ali <vilayatcodemysite@gmail.com>
2023-04-22 22:14:36 +0530

    added navbar links

---
+++ src/components/Navbar/options/index.ts
@@ -0,0 +1,19 @@
+export interface ITEM_TYPE {
+    title: string,
+    href: string
+}
+
+export const ITEMS: ITEM_TYPE[] = [
+    {  
+        title: "Home",
+        href: "/"
+    },
+    {  
+        title: "Play",
+        href: "/play"
+    },    
+    {  
+        title: "Dev Log",
+        href: "/"
+    },
+];
